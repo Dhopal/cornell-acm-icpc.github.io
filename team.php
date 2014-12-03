ca2e52fd34796c6be62896f88519a072a32c6b6f
Akhila Ananthram <asa225@cornell.edu>
2014-12-03 15:58:56 -0500

    team

---
+++ team.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Cornell ACM ICPC</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css" />
+	<link rel="stylesheet" type="text/css" href="style/team.css" />
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Noticia+Text' rel='stylesheet' type='text/css'>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+	<?php include("header.html"); ?>
+	<div id="content">
+		<h1>Team:</h1>
+
+		<div id="group1" class="sec">
+			<h3>Coaches</h3>
+			<p><a href="daniel.php">Daniel Fleischman</a></p>
+			<p><a href="saketh.php">Saketh Are</a></p>
+		</div>
+
+		<div id="group2" class="sec">
+			<h3>Members</h3>
+			<p><a href="members.php">Get to know the team</a></p>
+		</div>
+	</div>
+</body>
