38057f2214682ba85e538e03ed9962c9f1e581ab
Akhila Ananthram <asa225@cornell.edu>
2014-12-03 16:09:41 -0500

    info from daniel

--- daniel.php
+++ daniel.php
@@ -19,7 +19,14 @@
     <div id="content">
       <div id="coach">
         <h1>Daniel Fleischman</h1>
-        <p>Lorem Ipsum</p>
+        <p>I am a PhD student in the ORIE department with minors in CS and Applied Math.
+          I started competing on ACM in 2005, when I was in college and was hooked up on competitions since then.
+          I had to retire from ACM-ICPC after my second world final in 2007 (you can't compete after two WF), so I started to coach in 2009,
+          still in my home university, in Brazil. I've been the coach for Cornell since 2012, and I love it!
+          <br><br>
+          Even being retired from ACM-ICPC I can (and do) still compete on other programming competitions like Google CodeJam,
+          Facebook HackerCup and Topcoder Open. Other competitions that interest me are Mathematics competitions (like the IMC)
+          and puzzle competitions (like the Microsoft Puzzle Challenge).</p>
       </div>
     </div>
   </body>
